36e45b701241b16a1fad5f9ebf290e0d0383d0d4
Benson Kalahar <bensonk42@gmail.com>
2010-10-13 18:37:29 -0700

    Made it a ListView, for the purposes of awesome.

--- src/info/voidptr/nomads/NomadViewer.java
+++ src/info/voidptr/nomads/NomadViewer.java
@@ -3,9 +3,7 @@ package info.voidptr.nomads;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.Calendar;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -14,52 +12,73 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.net.http.AndroidHttpClient;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class NomadViewer extends Activity {
+public class NomadViewer extends ListActivity {
 	public static final String TAG = "NomadViewer";
 	private static String ACTIVITY_URL = "http://nomad.heroku.com/activity/plain.json";
 	private static String USERS_URL = "http://nomad.heroku.com/users.json";
 	private static String SUGGESTIONS_URL = "http://nomad.heroku.com/suggestions.json";
+	
+	private String[] links;
+	
+	// For later:
+	// private static String POSTS_URL = "http://nomad.heroku.com/posts.json";
+	// private static String COMMENTS_URL = "http://nomad.heroku.com/comments.json";
 
 	Handler handler;
 	DatabaseHelper db;
-
+	long lastUpdate = 0;
+	
     /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         handler = new Handler();
         db = new DatabaseHelper(this);
         setContentView(R.layout.main);
         fetchUpdates();
     }
+	
+	@Override
+	public void onListItemClick(ListView parent, View v, int i, long id) {
+		Log.i(TAG, "Opening url: " + links[i]);
+		Intent browserIntent = new Intent("android.intent.action.VIEW", Uri.parse(links[i]));
+		startActivity(browserIntent);
+	}
 
 	private void fetchUpdates() {
 		new Thread(new Runnable() { @Override
 		public void run() {
-			showText("Fetching activity from " + ACTIVITY_URL);
+			Log.i(TAG, "Fetching activity from " + ACTIVITY_URL);
 			AndroidHttpClient client = AndroidHttpClient.newInstance("Nomad App");
 
 			// Fetch recent activity
 			try {
 				HttpResponse res = client.execute((HttpUriRequest) new HttpGet(ACTIVITY_URL));
-				showText("Got statuses...");
 				BufferedReader reader = new BufferedReader(new InputStreamReader(res.getEntity().getContent()));
 				String actions = reader.readLine();
 
 				Log.i(TAG, "Actions: " + actions);
-				showText(formatActions(actions));
+				parseAndShowActions(actions);
 			} catch (IOException e) {
-				showText("Failure fetching recent activity: " + e.getClass().getName() + " -- " + e.getMessage());
-				Log.w(TAG, e.getMessage());
+				Log.e(TAG, e.getMessage());
 			}
+			
+			long time = Calendar.getInstance().getTimeInMillis();
+			long age = lastUpdate - time;
+			lastUpdate = time;
+			if(age < 10000) return;
 
 			// Fetch Users table
 			try {
@@ -72,12 +91,10 @@ public class NomadViewer extends Activity {
 					line = reader.readLine();
 				}
 				String json_str = content_buffer.toString();
-				json_str = json_str + "";
-
+				updateUsers(json_str);
 				Log.i(TAG, "Fetched Users table");
 			} catch (IOException e) {
-				showText("Failure fetching users: " + e.getClass().getName() + " -- " + e.getMessage());
-				Log.w(TAG, e.getMessage());
+				Log.e(TAG, e.getMessage());
 			}
 
 			// Fetch Suggestions table
@@ -93,18 +110,15 @@ public class NomadViewer extends Activity {
 				String json_str = content_buffer.toString();
 				updateSuggestions(json_str);
 			} catch (IOException e) {
-				showText("Failure: " + e.getClass().getName() + " -- " + e.getMessage());
-				Log.w(TAG, e.getMessage());
+				Log.e(TAG, e.getMessage());
 			}
 
 			client.close();
 		} }).start();
 	}
-	
-	private List<Map<String,String>> updateSuggestions(String json) {
-		List<Map<String,String>> ret = new LinkedList<Map<String,String>>();
-		SQLiteDatabase conn = db.getWritableDatabase();
 
+	private void updateSuggestions(String json) {
+		SQLiteDatabase conn = db.getWritableDatabase();
 		try {
 			JSONArray array = new JSONArray(json);
 			for(int i = 0; i < array.length(); i++) {
@@ -133,55 +147,77 @@ public class NomadViewer extends Activity {
 			               + "'" + content.replace("'", "''") + "', "
 			               + user_id
 			               + ")";
-				Log.i("inserting", title);
+				Log.i(TAG, "inserting suggestion " + title);
 				conn.execSQL(sql);
-				Log.i("inserted", title);
 			}
 		} catch (JSONException e) {
 			Log.w(TAG, "Failed to parse or insert suggestion: " + e.getClass().getName() + " -- " + e.getMessage());
 		}
-		finally {
-			conn.close();
+		catch (IllegalStateException e) {
+			Log.w(TAG, "Failure parsing suggestions: " + e.getClass().getName() + " -- " + e.getMessage());
 		}
-		return ret;
+		conn.close();
 	}
 
-	@SuppressWarnings("unused")
-	private String updateUsers(String json) {
-		String ret = "<error parsing suggestions>";
+	private void updateUsers(String json) {
+		SQLiteDatabase conn = db.getWritableDatabase();
 		try {
 			JSONArray array = new JSONArray(json);
-			ret = "";
 			for(int i = 0; i < array.length(); i++) {
 				JSONObject user = array.getJSONObject(i);
+				String id = user.getString("id");
+				String name = user.getString("name");
+				String created_at = user.getString("created_at");
+				String updated_at = user.getString("updated_at");
+				String fullname = user.getString("fullname");
+				String admin = (user.getString("admin").compareTo("true") == 0) ? "1" : "0";
+
+				String sql = "INSERT INTO users"
+				           + "(_id, name, created_at, updated_at, fullname, admin) "
+		                   + "values ("
+		                   + id + ", "
+		                   + "'" + name + "', "
+			               + "'" + created_at + "', "
+			               + "'" + updated_at + "', "
+			               + "'" + fullname + "', "
+		                   + admin
+		                   + ")";
+				Log.i(TAG, "inserting user " + name);
+				conn.execSQL(sql);
 			}
 		} catch (JSONException e) {
 			Log.w(TAG, "Failed to parse actions: " + e.getClass().getName() + " -- " + e.getMessage());
 		}
-		return ret;
+		catch (IllegalStateException e) {
+			Log.w(TAG, "Failure parsing users: " + e.getClass().getName() + " -- " + e.getMessage());
+		}
+		conn.close();
 	}
 
-	private String formatActions(String actions) {
-		String ret = "<error parsing actions>";
+	private void parseAndShowActions(String actions) {
+		String[] items = new String[] { "<error parsing actions>" };
 		try {
 			JSONArray json = new JSONArray(actions);
-			ret = "";
+			items = new String[json.length()];
+			links = new String[json.length()];
 			for(int i = 0; i < json.length(); i++) {
-				ret = ret + json.getString(i) + "\n\n";
+				JSONArray pair = json.getJSONArray(i);
+				items[i] = pair.getString(0);
+				links[i] = pair.getString(1);
 			}
+			showActions(items);
 		} catch (JSONException e) {
 			Log.w(TAG, "Failed to parse actions: " + e.getClass().getName() + " -- " + e.getMessage());
 		}
-		return ret;
 	}
 
-	private void showText(final String txt) {
+	private void showActions(final String[] txt) {
 		// Don't you just love Java? 
+		final ListActivity parent = this;
 		Runnable r = new Runnable() {
 			@Override
 			public void run() {
-				TextView display = (TextView) findViewById(R.id.display);
-				display.setText(txt);
+				setListAdapter(new ArrayAdapter<String>(parent, android.R.layout.simple_list_item_1, txt));
 			}
 		};
 		handler.post(r);
